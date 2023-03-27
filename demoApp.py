352fc73fb173bfe5c86bd0f580d184f16729462b
sandiaaVsivam <sandiaa.paramasivam@transferwise.com>
2023-03-27 15:17:27 +0100

    modified token

--- demoApp.py
+++ demoApp.py
@@ -4,15 +4,16 @@ from service import transferService
 
 class DemoApplication:
     def main(self):
-        # transferService.TransferService.generateToken()
+        token = transferService.TransferService.generateToken()
+        print(token)
         createQuoteRequest = self.getQuoteRequest()
-        response = transferService.TransferService.createQuote(createQuoteRequest)
+        response = transferService.TransferService.createQuote(createQuoteRequest, token)
         if(response.status_code != 200) :
             print("Quote creation failed")
         else:
             print("Created quote id ",response.json()["id"])
 
-        self.createRecipient(createQuoteRequest._targetCurrency)
+        self.createRecipient(createQuoteRequest._targetCurrency, token)
 
     def getQuoteRequest(self):
         sourceCurrency = input("From which currency would you like to transfer? Please enter the currency code. (e.g., GBP)")
@@ -27,7 +28,7 @@ class DemoApplication:
         return quote
     
 
-    def createRecipient(self, targetCurrency):
+    def createRecipient(self, targetCurrency, token):
         print("Please enter recipient account details:")
         accountNumber = input("Account number")
         email = input("Email")
@@ -43,7 +44,7 @@ class DemoApplication:
         details._accountType=accountType
         details._address=Address.Address()
         recipient._details=details
-        response = transferService.TransferService.createRecipient(recipient)
+        response = transferService.TransferService.createRecipient(recipient, token)
     
         if(response.status_code != 200) :
             print("Creation failed")
--- service/transferService.py
+++ service/transferService.py
@@ -5,40 +5,41 @@ from service.MyEncoder import MyEncoder
 
 
 class TransferService:
-    def generateToken(self):
+    def generateToken():
 
         url = 'https://api.sandbox.transferwise.tech/oauth/token'
         data = { 'grant_type': 'refresh_token',   
-          'refresh_token': '<>'}
+          'refresh_token': 'efd76f1c-f819-4138-b5f1-2fe69512cec7'}
 
 # Set headers including authorization token and content type
-        headers = {'Authorization': 'Basic <>',
+        headers = {'Authorization': 'Basic dHctdGVzdC1iYW5rOnR3LXRlc3QtYmFuaw==',
            'Content-Type': 'application/x-www-form-urlencoded'}
 
 # Make the POST request
         response = requests.post(url, headers=headers, data=data)
-        print(response.json())
+        print(response.json()['access_token'])
+        return (response.json()['access_token'])
 
 # Check the response status code and content
 
 
-    def createQuote(createQuoteRequest):
+    def createQuote(createQuoteRequest, token):
         print(vars(createQuoteRequest))
         json_string = json.dumps(createQuoteRequest, cls=MyEncoder)
         print(json_string)
         url = "https://api.sandbox.transferwise.tech/v2/quotes"
-        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer'}
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer '+token}
         
         response = requests.post(url, data=json_string, headers=headers)
         print(response)
-        return response.json()
+        return response
 
     
-    def createRecipient(createRecipientRequest):
+    def createRecipient(createRecipientRequest, token):
         json_string1 = json.dumps(createRecipientRequest, cls=MyEncoder)
         print(json_string1)
         url = "https://api.sandbox.transferwise.tech/v1/accounts"
-        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer'}
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer '+token}
 
         response = requests.post(url, data=json_string1, headers=headers)
         print(response)
